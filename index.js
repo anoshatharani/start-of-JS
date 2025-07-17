645e21b1b276f468871443f59b6bf7481b2c656d
anoshatharani <ano18sha@gmail.com>
2025-07-17 17:40:18 +0500

    arthematic

---
+++ index.js
@@ -0,0 +1,58 @@
+alert("hey good to see you")
+// console.log(alert)
+
+var yourname = "anosha" ; //  initialization
+alert(yourname); 
+console.log (yourname) ; // print
+yourname ="anoshatharani" // reasssign
+console.log (yourname)
+var fathername ;  // declaration
+console.log (fathername)  // it shows undefined
+fathername ="m.Amin"
+console.log (fathername) //  declaration become initialization here
+// -------------------------numbers of var
+var num = 18
+console.log (num+1)
+
+num = 18
+console.log (num+2)
+
+var numbers = 108
+console.log (numbers+5)
+
+num = num+2
+console.log (num)
+
+var HEY = num+10
+console.log (HEY)
+
+var num1 = 10
+var num2 = 50
+var add = num1+num2 
+console.log (add)
+
+
+// -----------------------day one has completed----------------------------
+var num = 20
+var num1 = 30
+console.log (num)
+var sub = num - num1
+console.log (sub)
+
+var multiply= num * num1
+console.log (multiply)
+
+var divide = num / num1
+console.log(divide)
+
+var modulas = num % num1
+console.log(modulas)
+
+var preinc = 4
+var result = ++preinc
+console.log(result)
+
+var postinc = 4
+var result = postinc++
+console.log(result)
+

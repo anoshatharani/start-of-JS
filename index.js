7a9948c962629e26964ead5d7f2523d8d167a5a2
anoshatharani <ano18sha@gmail.com>
2025-08-16 00:19:28 +0500

    practice

--- index.js
+++ index.js
@@ -1,58 +1,94 @@
-alert("hey good to see you")
-// console.log(alert)
+// alert("hey good to see you")
+// // console.log(alert)
 
-var yourname = "anosha" ; //  initialization
-alert(yourname); 
-console.log (yourname) ; // print
-yourname ="anoshatharani" // reasssign
-console.log (yourname)
-var fathername ;  // declaration
-console.log (fathername)  // it shows undefined
-fathername ="m.Amin"
-console.log (fathername) //  declaration become initialization here
-// -------------------------numbers of var
-var num = 18
-console.log (num+1)
+// var yourname = "anosha" ; //  initialization
+// alert(yourname); 
+// console.log (yourname) ; // print
+// yourname ="anoshatharani" // reasssign
+// console.log (yourname)
+// var fathername ;  // declaration
+// console.log (fathername)  // it shows undefined
+// fathername ="m.Amin"
+// console.log (fathername) //  declaration become initialization here
+// // -------------------------numbers of var
+// var num = 18
+// console.log (num+1)
 
-num = 18
-console.log (num+2)
+// num = 18
+// console.log (num+2)
 
-var numbers = 108
-console.log (numbers+5)
+// var numbers = 108
+// console.log (numbers+5)
 
-num = num+2
-console.log (num)
+// num = num+2
+// console.log (num)
 
-var HEY = num+10
-console.log (HEY)
+// var HEY = num+10
+// console.log (HEY)
 
-var num1 = 10
-var num2 = 50
-var add = num1+num2 
-console.log (add)
+// var num1 = 10
+// var num2 = 50
+// var add = num1+num2 
+// console.log (add)
 
 
-// -----------------------day one has completed----------------------------
-var num = 20
-var num1 = 30
-console.log (num)
-var sub = num - num1
-console.log (sub)
+// // -----------------------day one has completed----------------------------
+// var num = 20
+// var num1 = 30
+// console.log (num)
+// var sub = num - num1
+// console.log (sub)
 
-var multiply= num * num1
-console.log (multiply)
+// var multiply= num * num1
+// console.log (multiply)
 
-var divide = num / num1
-console.log(divide)
+// var divide = num / num1
+// console.log(divide)
 
-var modulas = num % num1
-console.log(modulas)
+// var modulas = num % num1
+// console.log(modulas)
 
-var preinc = 4
-var result = ++preinc
-console.log(result)
+// var preinc = 4
+// var result = ++preinc
+// console.log(result)
 
-var postinc = 4
-var result = postinc++
-console.log(result)
+// var postinc = 4
+// var result = postinc++
+// console.log(result)
+// // ------------------------------------------DAY TWO HAS COMPLETED-------------------------------------------
+// //  ---------------------------CONCATINATION-------------------------------
+// var name = "anosha"
+// var FatherName = "M.Amin"
+// var FullNmae = name + FatherName
+// console.log(FullNmae)
 
+// var num = "12345"
+// var num02 = "678910"
+// var FullNum = num + num02
+// console.log(FullNum)
+
+// console.log(3+3*"4"-2) //12 ,15,13
+// console.log(2-"2"+6+8) // 14 , 0
+// console.log(2+"2"+6-8) // 226 , 218
+// console,log(6+6+"32"+2-10) //12322 , 12312
+
+
+
+
+
+
+
+
+
+//practice chap-1to20
+//----------------------Primtive Datatypes of JS
+// nn bb ss u
+// let a = null
+// let b = 345
+// let c = true // can also be false
+// let d = BigInt("567") + BigInt("3")
+// let e = "anosha"
+// let f = Symbol("I am anice symbol")
+// let g = undefined
+// console.log(a,b,c,d,e,f,g)
+// console.log(typeof d)
